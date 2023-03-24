44f8dbc87d312e848d13894c1288353fb4acd76a
leewg97 <leewg97@naver.com>
2023-03-25 06:30:59 +0900

    Conf #11 : Add spring retry related settings

---
+++ src/main/java/com/example/project/config/RetryConfig.java
@@ -0,0 +1,15 @@
+package com.example.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+
+//    @Bean
+//    public RetryTemplate retryTemplate() {
+//        return new RetryTemplate();
+//    }
+
+}
